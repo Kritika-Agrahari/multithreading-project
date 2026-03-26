49d1a77d42cb83520090487941fd8656486f9a66
aash0617 <bhallaaastha76@gmail.com>
2026-03-26 19:17:27 +0530

    Implement TransactionThread class for threading

---
+++ transaction.py
@@ -0,0 +1,19 @@
+import threading
+
+class TransactionThread(threading.Thread):
+    def __init__(self, thread_id, account, txn_type, amount):
+        super().__init__()
+        self.thread_id = thread_id
+        self.account = account
+        self.txn_type = txn_type
+        self.amount = amount
+
+    def run(self):
+        print(f"[Thread {self.thread_id}] Started")
+
+        if self.txn_type == "deposit":
+            self.account.deposit(self.amount)
+        elif self.txn_type == "withdraw":
+            self.account.withdraw(self.amount)
+
+        print(f"[Thread {self.thread_id}] Finished")
