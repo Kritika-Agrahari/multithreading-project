76d61f46e9b0cae23bf307bec19f834ea4b56ce7
aash0617 <bhallaaastha76@gmail.com>
2026-03-26 20:10:59 +0530

    Enhance RoundRobinScheduler with UI update and context tracking
    Refactor RoundRobinScheduler to include Gantt chart and context tracking.

--- scheduler.py
+++ scheduler.py
@@ -1,65 +1,39 @@
 import time
-from bank_account import BankAccount
-from transaction import TransactionThread
 
 class RoundRobinScheduler:
     def __init__(self, time_quantum=1):
         self.queue = []
         self.time_quantum = time_quantum
-        self.context_switches = 0
+        self.context = 0
+        self.gantt = []
 
     def add_thread(self, thread):
+        thread.state = "READY"
         self.queue.append(thread)
-        print(f"[Scheduler] Thread {thread.thread_id} added to Ready Queue")
 
-    def run(self):
-        print("\n⚙️  Scheduler Starting...\n")
-
+    def run(self, update_ui):
         while self.queue:
             thread = self.queue.pop(0)
 
-            print(f"[CPU] ▶ Executing Thread {thread.thread_id} "
-                  f"({thread.txn_type} ₹{thread.amount})")
+            # mark running
+            thread.state = "RUNNING"
 
-            # Start only once
+            # start once
             if not hasattr(thread, "started"):
                 thread.start()
                 thread.started = True
 
-            # Give time slice
             thread.join(timeout=self.time_quantum)
 
-            self.context_switches += 1
-            print(f"[CPU] ↔ Context Switch #{self.context_switches}\n")
+            self.context += 1
+            self.gantt.append(f"T{thread.thread_id}")
 
-            # If still running → re-add to queue
+            # if not finished → back to queue
             if thread.is_alive():
+                thread.state = "READY"
                 self.queue.append(thread)
+            else:
+                thread.state = "TERMINATED"
 
-            time.sleep(0.2)
-
-        print(f"\n✅ All threads executed.")
-        print(f"📊 Total Context Switches: {self.context_switches}")
-
-def main():
-    account = BankAccount("ACC123", 1000)
-
-    scheduler = RoundRobinScheduler(time_quantum=1)
-
-    threads = [
-        TransactionThread(1, account, "deposit", 500),
-        TransactionThread(2, account, "withdraw", 700),
-        TransactionThread(3, account, "withdraw", 300),
-    ]
-
-    for t in threads:
-        scheduler.add_thread(t)
-
-    scheduler.run()
-
-    print("\nFinal Balance:", account.balance)
-    account.print_log()
-
-
-if __name__ == "__main__":
-    main()
+            update_ui()
+            time.sleep(0.8)
